33c9388a0c2c904425bec59188e8ea9b9b73ebfd
David Divilly <d.divilly@gmail.com>
2017-08-24 16:01:44 +0100

    changed ENV

--- models/link.rb
+++ models/link.rb
@@ -9,7 +9,7 @@ class Link
   property :title, String
   property :url, String
 
-  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}") #decides wich database to go depending on command in file ENV['RACK_ENV'] = 'test'/ ENV["RACK_ENV"] ||= 'development'
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}") #decides wich database to go depending on command in file ENV['RACK_ENV'] = 'test'/ ENV["RACK_ENV"] ||= 'development'
   DataMapper.finalize
   DataMapper.auto_upgrade!
 end
